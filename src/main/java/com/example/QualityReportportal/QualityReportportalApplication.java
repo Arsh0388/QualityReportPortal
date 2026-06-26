3b48d5fb0fd97c02f5a34c172355502e1be51c62
ArshArora <arsharora0388@gmail.com>
2026-06-26 01:14:27 -0600

    creating a java spring Report Portal Project - full stack

---
+++ src/main/java/com/example/QualityReportportal/QualityReportportalApplication.java
@@ -0,0 +1,13 @@
+package com.example.QualityReportportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualityReportportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualityReportportalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/QualityReportportal/QualityReportportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QualityReportportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualityReportportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
